5d906d826ce04870cdb299346139d4cc8a232f4b
Efraim Vander <efraim.vander@omnidata.co.ao>
2026-04-12 20:19:32 +0100

    primeiro commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    nome = os.getenv("NOME", "DevOps")
+    return f"Olá {nome} 🚀"
+
+app.run(host="0.0.0.0", port=5000)
