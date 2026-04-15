354ca9106e99184300de8c2c416543e046217c57
Efraim Vander <efraim.vander@omnidata.co.ao>
2026-04-15 15:05:29 +0100

    altering app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     nome = os.getenv("NOME", "DevOps")
-    return f"Olá {nome} 🚀"
+    return f"Olá {nome}"
 
 app.run(host="0.0.0.0", port=5000)
